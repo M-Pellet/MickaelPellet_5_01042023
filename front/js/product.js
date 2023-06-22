c900ec08f8048834c201e7e43b70000f5dab58aa
Hugo STK <hugobrionne@gmail.com>
2023-06-22 18:57:25 +0900

    id pour page product session mentorat

--- front/js/product.js
+++ front/js/product.js
@@ -2,3 +2,7 @@ async function asyncData(){
     const productID = getProductId()
 
 }
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+const urlId = 'http://localhost:3000/api/products' + `/${id}`
