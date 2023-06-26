fa7262d085aaa23de1d8670a1386f1976cd6c458
Hugo STK <hugobrionne@gmail.com>
2023-06-26 14:02:47 +0900

    session mentorat

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,14 @@
-async function asyncData(){
-    const productID = getProductId()
+// async function asyncData(){
+//     const productID = getProductId()
 
-}
+// }
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 const urlId = 'http://localhost:3000/api/products' + `/${id}`
+
+
+// PANIER (LocalStoprage)
+// - Ajouter les items dans un tableau avec l'option distincte (couleur)
+// - Stocker les informations du produit (reprendre les valeurs indiquées dans le panier)
+// - Gérer les quantitées si on rajoute des quantités sur le même produit (incrémentation du nombre de produit)
